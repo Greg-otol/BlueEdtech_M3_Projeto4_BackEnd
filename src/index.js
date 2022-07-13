419ef04f2e57c7785f0500eda0813a721a96999b
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2022-07-12 21:46:04 -0300

    chore: config index

---
+++ src/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectionDB = require('./database/database');
+const userRoute = require("./users/users.route");
+const authRoute = require("./auth/auth.route");
+const character = require("./rickandmorty/rickandmorty.route");
+
+const port = process.env.PORT || 3001;
+const application = express();
+
+connectionDB();
+
+application.use(cors());
+application.use(express.json());
+
+application.use("/users", userRoute);
+application.use("/auth", authRoute);
+application.use("/characters", character);
+
+
+application.listen(port, () => {
+  console.log(`Server running on the port ${port}`);
+});
