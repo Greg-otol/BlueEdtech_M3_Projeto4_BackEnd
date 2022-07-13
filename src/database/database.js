aa691f5d5b97fd21477b7404345feabec11766b3
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2022-07-12 21:46:44 -0300

    chore: connection db

---
+++ src/database/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const connection = () => {
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB!"))
+    .catch((err) => console.log(`Not connected to MongoDB, erro: ${err}`));
+};
+
+module.exports = connection;
